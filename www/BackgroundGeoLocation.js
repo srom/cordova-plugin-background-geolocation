18a012af68cf990339444bc74076562d73c03eb2
srom <romain.strock@gmail.com>
2014-08-27 16:26:07 +0100

    Remplace <js-module> by <asset>

--- www/BackgroundGeoLocation.js
+++ www/BackgroundGeoLocation.js
@@ -1,66 +1,65 @@
-var backgroundGeoLocation = function() {};
+window.backgroundGeoLocation = {
+  configure: function(success, failure, config) {
+    var params            = JSON.stringify(config.params || {}),
+        headers           = JSON.stringify(config.headers || {}),
+        url               = config.url || 'BackgroundGeoLocation_url',
+        stationaryRadius  = (config.stationaryRadius >= 0) ? config.stationaryRadius : 50,  // meters
+        distanceFilter    = (config.distanceFilter >= 0) ? config.distanceFilter : 500,     // meters
+        locationTimeout   = (config.locationTimeout >= 0) ? config.locationTimeout : 60,    // seconds
+        desiredAccuracy   = (config.desiredAccuracy >= 0) ? config.desiredAccuracy : 100,   // meters
+        debug             = config.debug || false,
+        notificationTitle = config.notificationTitle || "Background tracking",
+        notificationText  = config.notificationText || "ENABLED",
+        activityType      = config.activityType || "OTHER";
 
-backgroundGeoLocation.prototype.configure = function(success, failure, config) {
-  var params            = JSON.stringify(config.params || {}),
-      headers           = JSON.stringify(config.headers || {}),
-      url               = config.url        || 'BackgroundGeoLocation_url',
-      stationaryRadius  = (config.stationaryRadius >= 0) ? config.stationaryRadius : 50,  // meters
-      distanceFilter    = (config.distanceFilter >= 0) ? config.distanceFilter : 500,     // meters
-      locationTimeout   = (config.locationTimeout >= 0) ? config.locationTimeout : 60,    // seconds
-      desiredAccuracy   = (config.desiredAccuracy >= 0) ? config.desiredAccuracy : 100,   // meters
-      debug             = config.debug || false,
-      notificationTitle = config.notificationTitle || "Background tracking",
-      notificationText  = config.notificationText || "ENABLED",
-      activityType      = config.activityType || "OTHER";
+    cordova.exec(
+      success || function() {},
+      failure || function() {},
+      'BackgroundGeoLocation',
+      'configure',
+      [params, headers, url, stationaryRadius, distanceFilter, locationTimeout, desiredAccuracy, debug, notificationTitle, notificationText, activityType]
+    );
+  },
 
-  cordova.exec(
-    success || function() {},
-    failure || function() {},
-    'BackgroundGeoLocation',
-    'configure',
-    [params, headers, url, stationaryRadius, distanceFilter, locationTimeout, desiredAccuracy, debug, notificationTitle, notificationText, activityType]
-  );
-};
+  start: function(success, failure, config) {
+    cordova.exec(
+      success || function() {},
+      failure || function() {},
+      'BackgroundGeoLocation',
+      'start',
+      []
+    );
+  },
 
-backgroundGeoLocation.prototype.start = function(success, failure, config) {
-  cordova.exec(
-    success || function() {},
-    failure || function() {},
-    'BackgroundGeoLocation',
-    'start',
-    []
-  );
-};
+  stop: function(success, failure, config) {
+    cordova.exec(
+      success || function() {},
+      failure || function() {},
+      'BackgroundGeoLocation',
+      'stop',
+      []
+    );
+  },
 
-backgroundGeoLocation.prototype.stop = function(success, failure, config) {
-  cordova.exec(
-    success || function() {},
-    failure || function() {},
-    'BackgroundGeoLocation',
-    'stop',
-    []
-  );
-};
+  finish: function(success, failure) {
+    cordova.exec(
+      success || function() {},
+      failure || function() {},
+      'BackgroundGeoLocation',
+      'finish',
+      []
+    );
+  },
 
-backgroundGeoLocation.prototype.finish = function(success, failure) {
-  cordova.exec(
-    success || function() {},
-    failure || function() {},
-    'BackgroundGeoLocation',
-    'finish',
-    []
-  );
-};
-
-backgroundGeoLocation.prototype.changePace = function(isMoving, success, failure) {
-  cordova.exec(
-    success || function() {},
-    failure || function() {},
-    'BackgroundGeoLocation',
-    'onPaceChange',
-    [isMoving]
-  );
-};
+  changePace: function(isMoving, success, failure) {
+    cordova.exec(
+      success || function() {},
+      failure || function() {},
+      'BackgroundGeoLocation',
+      'onPaceChange',
+      [isMoving]
+    );
+  },
 
 /**
 * @param {Integer} stationaryRadius
@@ -68,37 +67,26 @@ backgroundGeoLocation.prototype.changePace = function(isMoving, success, failure
 * @param {Integer} distanceFilter
 * @param {Integer} timeout
 */
-backgroundGeoLocation.prototype.setConfig = function(success, failure, config) {
-  cordova.exec(
-    success || function() {},
-    failure || function() {},
-    'BackgroundGeoLocation',
-    'setConfig',
-    [config]
-  );
-};
+  setConfig: function(success, failure, config) {
+    cordova.exec(
+      success || function() {},
+      failure || function() {},
+      'BackgroundGeoLocation',
+      'setConfig',
+      [config]
+    );
+  },
 
 /**
 * Returns current stationaryLocation if available.  null if not
 */
-backgroundGeoLocation.prototype.getStationaryLocation = function(success, failure) {
-  cordova.exec(
-    success || function() {},
-    failure || function() {},
-    'BackgroundGeoLocation',
-    'getStationaryLocation',
-    []
-  );
+  getStationaryLocation: function(success, failure) {
+    cordova.exec(
+      success || function() {},
+      failure || function() {},
+      'BackgroundGeoLocation',
+      'getStationaryLocation',
+      []
+    );
+  }
 };
-
-// Plug in to Cordova
-// cordova.addConstructor(function() {
-//   if (!window.plugins) {
-//     window.plugins = {};
-//   }
-//   window.plugins.backgroundGeoLocation = new backgroundGeoLocation();
-// });
-// if (!window.plugins) {
-//   window.plugins = {};
-// }
-window.backgroundGeoLocation = new backgroundGeoLocation();
